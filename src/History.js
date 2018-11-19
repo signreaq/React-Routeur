b3aa214b5da8c831808536a59b12833eecfd980d
Thomas Lambert <lambertthomas10@gmail.com>
2018-11-19 09:27:29 +0100

    fin react routeur

---
+++ src/History.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const History = function(){
+    return(
+    <div>
+        <h1>Notre histoire</h1>
+    </div>
+    );
+}
+
+export default History;
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = function(){
+    return(
+    <div>
+        <h1>Bienvenue sur l'accueil</h1>
+    </div>
+    );
+}
+
+export default Home;
